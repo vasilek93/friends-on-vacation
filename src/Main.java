296c518d857785e71bac892aec275eb8b71e127e
Alina Vasilevska <vasilek93@inbox.lv>
2016-07-04 20:41:17 +0300

    added fibonachi method

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,13 @@
  * Created by alina on 04.07.2016..
  */
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Test");
+    }
+
+    public void fibonachi(){
+
+
+    }
 }
